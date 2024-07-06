8ead75e0f10a287a1217d797d65cd0fafda9deef
talha-dev-pro <talha.dev.pro@gmail.com>
2024-07-07 01:05:52 +0500

    designed login and sign up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import Login from "./Login";
+import SignUp from "./SignUp";
+
 function App() {
+  const [login, setLogin] = useState(false);
+
+  const updateLogin = (e) => {
+    console.log(e);
+    setLogin(e);
+  };
+
   return (
     <>
-      <h1>Hello</h1>
+      <div className="w-screen h-screen">
+        {login ? (
+          <Login updateLogin={updateLogin} />
+        ) : (
+          <SignUp updateLogin={updateLogin} />
+        )}
+      </div>
     </>
   );
 }
---
+++ src/SignUp.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+
+const SignUp = ({ updateLogin }) => {
+  return (
+    <>
+      <div className=" w-full h-full bg-gray-400 flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center h-2/3 w-1/5 bg-white rounded-md">
+          <div>
+            <h1 className="text-3xl font-bold py-6">{"Sign Up Form"}</h1>
+          </div>
+          <form className="flex flex-col items-start justify-start px-2 py-2 w-full h-6/7">
+            <label className=" font-semibold">{"Email: "}</label>
+            <input
+              type="text"
+              className="w-full h-10 rounded-md border-2 border-gray-300 bg-gray-200"
+              required
+              placeholder="Enter Email"
+            />
+            <label className=" font-semibold">{"Password: "}</label>
+            <input
+              type="password"
+              className="w-full h-10 rounded-md border-2 border-gray-300 bg-gray-200 "
+              required
+              placeholder="Enter password"
+            />
+            <label className=" font-semibold">{"Confirm Password: "}</label>
+            <input
+              type="password"
+              className="w-full h-10 rounded-md border-2 border-gray-300 bg-gray-200"
+              required
+              placeholder="Confirm pssword"
+            />
+          </form>
+          <div className="flex justify-start flex-wrap w-full px-2">
+            <p>
+              {"Already have an account? "}
+              <span
+                className="font-semibold hover:underline hover:text-purple-400 cursor-pointer"
+                onClick={() => {
+                  updateLogin(true);
+                }}
+              >
+                {"Login"}
+              </span>
+            </p>
+          </div>
+          <div className="w-full h-1/5 flex items-center justify-center">
+            <button className=" bg-purple-400 rounded-md px-4 py-2 w-4/5 text-white">
+              {"SignUp"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+SignUp.propTypes = {
+  updateLogin: PropTypes.func,
+};
+
+export default SignUp;
